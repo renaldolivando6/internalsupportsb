aff6e8d74485c5f31810526b06517f643547adfc
Renaldo Livando <renaldolivando6@gmail.com>
2025-07-24 10:51:24 +0700

    Update Database map

--- config/database_map.php
+++ config/database_map.php
@@ -1,4 +1,4 @@
-<?php
+          <?php
 
 return [
     'company_connections' => [
@@ -9,7 +9,7 @@ return [
         'UPS1' => 'sqlsrv_195', 'UPJR' => 'sqlsrv_195', 'UPKD' => 'sqlsrv_195', 'UPBL' => 'sqlsrv_195', 'UPML' => 'sqlsrv_195',
         'UPPL' => 'sqlsrv_199', 'UPLP' => 'sqlsrv_199', 'UPMD' => 'sqlsrv_199', 'UPRI' => 'sqlsrv_199',
         'UPPD' => 'sqlsrv_199', 'UPBK' => 'sqlsrv_199', 'DPLL' => 'sqlsrv_199', 'UPJB' => 'sqlsrv_199', 'DPMB' => 'sqlsrv_199',
-        'TEST' => 'sqlsrv_194', 'UPP1' => 'sqlsrv_194', 'UPAM' => 'sqlsrv_194',
+        'TEST' => 'sqlsrv_194', 'UPP1' => 'sqlsrv_194', 'UPAM' => 'sqlsrv_194', 'UPGL' => 'sqlsrv_194',
 
         // Mapping Company IM
         // sqlsrv_194
@@ -25,8 +25,8 @@ return [
         'GPJ1' => 'sqlsrv_194', 'GPJP' => 'sqlsrv_194', 'GSBG' => 'sqlsrv_194', 'GSBH' => 'sqlsrv_194',
         'GSMG' => 'sqlsrv_194', 'GSMN' => 'sqlsrv_194', 'GSON' => 'sqlsrv_194', 'GSRG' => 'sqlsrv_194',
         'GTEI' => 'sqlsrv_194', 'GCKR' => 'sqlsrv_194',
-        'VTRD' => 'sqlsrv_194',
-
+        'VTRD' => 'sqlsrv_194', 'BSSJ' => 'sqlsrv_194',
+  
         // sqlsrv_196_1
         'GBSC' => 'sqlsrv_196_1', 'GDPK' => 'sqlsrv_196_1', 'GDPT' => 'sqlsrv_196_1', 'GPWK' => 'sqlsrv_196_1',
         'GSBM' => 'sqlsrv_196_1', 'GSLO' => 'sqlsrv_196_1', 'GTSK' => 'sqlsrv_196_1', 'GUPB' => 'sqlsrv_196_1',
@@ -37,7 +37,8 @@ return [
         // sqlsrv_199
         'GBLN' => 'sqlsrv_199', 'GBSM' => 'sqlsrv_199', 'GDPD' => 'sqlsrv_199', 'GDRK' => 'sqlsrv_199',
         'GGDS' => 'sqlsrv_199', 'GLHO' => 'sqlsrv_199', 'GLHT' => 'sqlsrv_199', 'GLLG' => 'sqlsrv_199',
-        'GPLT' => 'sqlsrv_199', 'GSWL' => 'sqlsrv_199', 'GTLB' => 'sqlsrv_199',
+        'GPLT' => 'sqlsrv_199', 'GSWL' => 'sqlsrv_199', 'GTLB' => 'sqlsrv_199', 'BNPP' => 'sqlsrv_199',
+        'PSSH' => 'sqlsrv_199',
 
         // sqlsrv_195
         'GBJN' => 'sqlsrv_195', 'GBL2' => 'sqlsrv_195', 'GBL3' => 'sqlsrv_195', 'GBLI' => 'sqlsrv_195',
